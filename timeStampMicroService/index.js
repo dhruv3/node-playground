702c66eed5f6e7953a58c355b22747311ec1f9ff
dhruv3 <dhruv.mnit@gmail.com>
2019-03-27 11:42:07 -0500

    Implementing homepage and initial js parts

--- timeStampMicroService/index.js
+++ timeStampMicroService/index.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res){
+    var absolutePath = __dirname + "/homePage.html"
+    res.sendFile(absolutePath) 
+})
+
 app.get('/api/timestamp/:date_string?', function(req, res){
-    res.end(JSON.stringify(req.params) + "\n" + JSON.stringify(req.query) + "\n" + JSON.stringify(req.route)) 
+    const date_string = req.params.date_string;
+    let obj = new Date(parseInt(date_string));
+    //check required to handle "2015-10-03" format dates
+    if(date_string.indexOf("-") != -1){
+        obj = new Date(date_string);
+    }
+    const retObj = {
+        "unix": obj.getTime(),
+        "utc": obj.toUTCString()
+    }
+    res.end(JSON.stringify(retObj));
+    // res.end(JSON.stringify(req.params) + "\n" + JSON.stringify(req.query) + "\n" + JSON.stringify(req.route)) 
 })
 // app.use(function(req, res){
     
