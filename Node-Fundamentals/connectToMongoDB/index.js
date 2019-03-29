b48c15727e5e39434de59fd9235f2ea7cbc0a26f
dhruv3 <dhruv.mnit@gmail.com>
2019-03-29 10:52:28 -0500

    adding code to reach online db

--- Node-Fundamentals/connectToMongoDB/index.js
+++ Node-Fundamentals/connectToMongoDB/index.js
@@ -10,3 +10,20 @@ mongoCient1.connect(url, function(err, db){
     console.log("database created");
     db.close();
 });
+
+// //Connecting to online Database
+// //https://stackoverflow.com/a/47662979
+// const mongoCient1 = require('mongodb').MongoClient;
+// //get the url from Mongo Atlas
+// //add the password for `admin`
+// //change db from test to whatever you've named your db
+// const url = "mongodb+srv://admin:admin@fcc-cluster-kuacf.mongodb.net/NewDBFCC?retryWrites=true";
+// mongoCient1.connect(url, function(err, client){
+//     if(err)
+//         throw err;
+//     console.log("database created");
+//     const db = client.db('NewDBFCC');
+//     db.collection('NewCollectionFCC').insertOne({name: 'Roger'}, (err, result) => {
+
+//     })
+// });
