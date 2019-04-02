10e44ba18f2bfdc7a10fc41b291017d5146bb06d
dhruv3 <dhruv.mnit@gmail.com>
2019-04-02 15:29:16 -0500

    client and server done

--- Node-FilesSocketNetwork/nodeNetworking/client.js
+++ Node-FilesSocketNetwork/nodeNetworking/client.js
@@ -11,4 +11,19 @@ const clientSocket = net.connect(options);
 
 clientSocket.on("connect", function(){
     console.log("Client connected successfully");
+    const message= "This is my message to the server.";
+    let dataSent = clientSocket.write(message, 'utf-8', function(){
+        console.log("Data sent to server");
+        console.log("Data sent to server: " + clientSocket.bytesWritten);
+    })
+    //check if the buffer is completely empty
+    clientSocket.on('drain', function(){
+        console.log("Buffer is completely empty");
+    })
+    //setup TCP client to receive data from server
+    clientSocket.on('data', function(data){
+        console.log("Data received from server: " + data);
+        //prop below tells how many bytes read from the client
+        console.log("Data read from server: " + clientSocket.bytesRead);
+    })
 })
