197a2893f2af772b45eed58f54737a8dfd837b9c
dhruv3 <dhruv.mnit@gmail.com>
2019-04-03 22:57:36 -0500

    one post op done. fields added in html

--- exerciseTracker/server/server.js
+++ exerciseTracker/server/server.js
@@ -8,13 +8,16 @@ var path = require('path');
 
 const mongoCient = require('mongodb').MongoClient;
 const url = "mongodb+srv://admin:admin@fcc-cluster-kuacf.mongodb.net/NewDBFCC?retryWrites=true";
-let collection = null;
+let userCollection = null;
+let userRecordCollection = null;
 mongoCient.connect(url, function(err, client){
     if(err)
         throw err;
     const db = client.db('NewDBFCC');
-    collection = db.collection('ExerciseTrackerCollection');
-    collection.remove({});
+    userCollection = db.collection('userExTrackerCollection');
+    userCollection.remove({});
+    userRecordCollection = db.collection('userRecordExTrackerCollection');
+    userRecordCollection.remove({});
 });
 
 const app = express()
@@ -25,6 +28,27 @@ const app = express()
 app.get('/', function(req, res){
     //https://stackoverflow.com/questions/14594121/express-res-sendfile-throwing-forbidden-error
     res.render(path.resolve(__dirname + "/../client/homePage.ejs"));
+})
+
+app.post('/api/exercise/new-user', function(req,res){
+    const newUserID = req.body.newUser;
+    userCollection.update(
+        { userID: newUserID },
+        { upsert: true }
+    ).then(function(data){
+        //update success message
+        if(data.result.upserted != undefined && data.result.nModified == 0){
+            res.end("New user '" + req.body.newUser + "' added!");
+        }
+        //update failed as it exists
+        else{
+            res.end("Username '" + req.body.newUser + "' already taken. Please try new one!");
+        }
+    })
+})
+
+app.post('/api/exercise/add', function(req, res){
+
 })
 
 app.set('view engine', 'ejs');
