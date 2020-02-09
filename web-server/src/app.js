4e9213dfbdc2db37307f0073760394dfd747cdb5
dhruv3 <dhruv.mnit@gmail.com>
2020-02-09 12:35:38 -0600

    Adding handlebar stuff

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,25 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: "Weather App",
+        name: "Dhruv"
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: "Help Me!"
+    });
+})
 
 app.get('/weather', (req, res) => {
     res.send({
