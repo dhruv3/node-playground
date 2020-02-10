6d61f05d2c4a8924bb38697e808675e68a99012f
dhruv3 <dhruv.mnit@gmail.com>
2020-02-10 13:55:14 -0600

    Adding partials and hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define path for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, "../templates/views")
+const partialsPath = path.join(__dirname, "../templates/partials")
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static dir to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -15,12 +25,17 @@ app.get('', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', {
+        title: "About",
+        name: "Dhruv"
+    });
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: "Help Me!"
+        helpText: "Help Me!",
+        title: "Help",
+        name: "Dhruv"
     });
 })
 
